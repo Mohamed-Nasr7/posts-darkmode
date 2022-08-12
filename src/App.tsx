1f35028ac202a6263dac9f76779be95025c11db8
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 06:55:22 +0200

    Add a dark button inside the li element

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ const App = () => {
   return (
     <div className='app'>
       <ul className='nav justify-content-end'>
-        <li className='nav-item'></li>
+        <li className='nav-item'>
+          <button type='button' className='btn btn-dark'>
+            Dark Mode
+          </button>
+        </li>
       </ul>
     </div>
   );
