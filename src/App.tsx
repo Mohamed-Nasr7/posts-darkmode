e852606465b54832e6763fbfcc5dadb5f204c1a3
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 11:54:21 +0200

    Create onToggleThemeClick function that sets the theme state

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,19 @@ const App = () => {
     localStorage.getItem('theme') || 'light'
   );
 
+  const onToggleThemeClick = () => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <div className='app' data-theme={theme}>
       <ul className='nav justify-content-end p-2'>
         <li className='nav-item'>
-          <button type='button' className='btn btn-dark'>
+          <button
+            type='button'
+            className='btn btn-dark'
+            onClick={onToggleThemeClick}
+          >
             Dark Mode
           </button>
         </li>
