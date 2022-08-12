6d52d72fa6b014386a2e6dc79d3eb6ab3ff43195
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 10:13:48 +0200

    Handle all postReducer action types

--- src/redux/reducers/postReducer.ts
+++ src/redux/reducers/postReducer.ts
@@ -4,4 +4,20 @@ const initialState = {
   postBody: '',
 };
 
-export const postReducer = (state = initialState, action: any) => {};
+export const postReducer = (state = initialState, action: any) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'POST_ID':
+      return { ...state, postId: payload };
+
+    case 'POST_TITLE':
+      return { ...state, postTitle: payload };
+
+    case 'POST_BODY':
+      return { ...state, postBody: payload };
+
+    default:
+      return state;
+  }
+};
