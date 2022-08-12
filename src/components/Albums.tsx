0d98ddfa994ff6e473d890bfeca78d7731fc02df
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 16:01:48 +0200

    Create a state isLoading and import baseUrl inside Albums component

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import { baseUrl } from '../constants/url';
 
 const Albums = () => {
   const [albums, setAlbums] = useState([]);
   const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   return <div></div>;
 };
