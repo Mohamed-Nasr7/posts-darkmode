38c933421dbbcbdc2048bb2733f97360db93692a
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 07:56:29 +0200

    Create an async function getData inside the Posts component useEffect that uses the fetchApi to get the posts data and sets the component states

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -6,6 +6,25 @@ const Posts = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
+        if (!response.ok) throw new Error();
+        const data = await response.json();
+        setPosts(data);
+      } catch {
+        setError('An error occured!! please try again.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return <div></div>;
 };
 
