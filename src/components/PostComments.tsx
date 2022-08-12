2503a8a2d637c59da8efb246bd715809a2a66610
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 10:16:09 +0200

    Call useSelector to get the stored post information

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const PostComments = () => {
+  const post = useSelector((state: any) => state);
+  console.log(post);
+
   return <div></div>;
 };
 
