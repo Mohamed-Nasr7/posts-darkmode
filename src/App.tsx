6bc6042344e14cb13a8f124803f91199296e439a
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 06:54:55 +0200

    Add a ul element and a child of li with bootstrap classes Inside the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <ul className='nav justify-content-end'>
+        <li className='nav-item'></li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
