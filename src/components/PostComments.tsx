40625eb801865226de478b5502b665021ed529ed
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 10:23:55 +0200

    Add a bootstrap card element with title and body

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -5,7 +5,19 @@ const PostComments = () => {
   const post = useSelector((state: any) => state);
   console.log(post);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title'>Card title</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostComments;
