1d645f7a68d4266d351df3ae32c0175e111c2cca
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-13 00:04:02 +0200

    Wrap the nav around the BrowserRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -19,32 +19,32 @@ const App = () => {
 
   return (
     <div className='app' data-theme={theme}>
-      <nav className='navbar sticky-top navbar-light bg-light'>
-        <ul className='navbar-nav w-100 flex-row justify-content-between p-2'>
-          <li className='nav-item'>
-            <ul className='navbar-nav w-100 flex-row p-2'>
-              <li className='nav-item'>
-                <NavLink to='/'>Posts</NavLink>
-              </li>
-              <li className='nav-item'>
-                <NavLink to='/albums'>Albums</NavLink>
-              </li>
-            </ul>
-          </li>
-
-          <li className='nav-item'>
-            <button
-              type='button'
-              className='btn btn-dark'
-              onClick={onToggleThemeClick}
-            >
-              {theme === 'light' ? 'Dark' : 'Light'} Mode
-            </button>
-          </li>
-        </ul>
-      </nav>
-
       <BrowserRouter>
+        <nav className='navbar sticky-top navbar-light bg-light'>
+          <ul className='navbar-nav w-100 flex-row justify-content-between p-2'>
+            <li className='nav-item'>
+              <ul className='navbar-nav w-100 flex-row p-2'>
+                <li className='nav-item'>
+                  <NavLink to='/'>Posts</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink to='/albums'>Albums</NavLink>
+                </li>
+              </ul>
+            </li>
+
+            <li className='nav-item'>
+              <button
+                type='button'
+                className='btn btn-dark'
+                onClick={onToggleThemeClick}
+              >
+                {theme === 'light' ? 'Dark' : 'Light'} Mode
+              </button>
+            </li>
+          </ul>
+        </nav>
+
         <Routes>
           <Route path='/' element={<Posts />} />
           <Route path='post/:id' element={<PostComments />} />
