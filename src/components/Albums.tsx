37a9863638c90107113232345140e43e7dee2bc9
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 15:46:53 +0200

    Create two states albums and photos insideAlbums.tsx

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 
 const Albums = () => {
+  const [albums, setAlbums] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
   return <div></div>;
 };
 
