1c707a3266b07fb9226eafce1f161103ea6c3ae4
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 10:30:19 +0200

    Render the post title and body values of the store in the card elements

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -9,11 +9,8 @@ const PostComments = () => {
     <div>
       <div className='card'>
         <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 className='card-title'>{post.title}</h5>
+          <p className='card-text'>{post.body}</p>
         </div>
       </div>
     </div>
