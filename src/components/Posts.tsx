2ffb6b3f6c3048fc4b1d7f6930df38f193042076
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 08:07:39 +0200

    Add a table template with rows and columns inside Posts component

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -25,7 +25,29 @@ const Posts = () => {
     getData();
   }, []);
 
-  return <div></div>;
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th scope='col'>First</th>
+          <th scope='col'>Last</th>
+          <th scope='col'>Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default Posts;
