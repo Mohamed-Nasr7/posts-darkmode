654bc6f42a18159ffc308341711c86598081b5b5
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 07:49:01 +0200

    Create three states of posts, isLoading, and error inside Posts component

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Posts = () => {
+  const [posts, setPosts] = useState<object | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   return <div></div>;
 };
 
