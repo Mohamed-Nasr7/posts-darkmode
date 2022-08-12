1aac7ffec34818c57989378d21061660e9266439
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-08-12 08:56:17 +0200

    Import Provider, createStore, and postReducer into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { postReducer } from './redux/reducers/postReducer';
 import App from './App';
 import './index.css';
 
